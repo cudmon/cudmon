0b40fcaac6c986a13fa4cef547b9f149484610b7
Tonsak <tonsak.gs@gmail.com>
2024-06-26 13:12:32 +0700

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Jost", "sans-serif"],
+    },
+  },
+} satisfies Config;
